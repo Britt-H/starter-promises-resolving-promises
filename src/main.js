60bbf94ffac6d8ca0c8220b1465c030a5833a3a2
Britt-H <brittain.hoover@gmail.com>
2023-03-15 23:17:23 -0400

    Correcting main.js to resolve correctly

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
+const question = "Will I be able to complete the 13 assessment?";
+const tellPromise = tell(question);
 
-welcome().then(console.log)
+welcome()
+  .then(console.log)
+  .then(goodbye)
+  .then(console.log)
+  .catch(console.error);
+tellPromise.then((fortune) => {
+  console.log(question);
+  console.log(fortune);
+});
